38b23da807685335f7cb7139a7b3a498f708e483
joseespinheira <joseespinheira@gmail.com>
2018-01-30 21:20:49 +0000

    Pagina 123, a cada dia um pouco mais

--- Aprendizado/Rest/vota_prato/config/routes.rb
+++ Aprendizado/Rest/vota_prato/config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    match 'categorias/:nome', controller: 'categorias', action: 'show', via: 'get'
+    match 'inicio' => 'restaurantes#index', via: 'put'
+    match 'inicio' => 'restaurantes#index', via: 'posst'
+    match 'inicio' => 'restaurantes#delet', via: 'delet'
+    
+    
+    resources :restaurantes
+    #Ao mapear o recurso :restaurantes, o rails automaticamente cria as seguintes rotas:
+    #• GET /restaurantes
+    #:controller => 'restaurantes', :action => 'index'
+    #• POST /restaurantes
+    #:controller => 'restaurantes', :action => 'create'
+    #• GET /restaurantes/new
+    #:controller => 'restaurantes', :action => 'new'
+    #• GET /restaurantes/:id
+    #:controller => 'restaurantes', :action => 'show'
+    #• PUT /restaurantes/:id
+    #:controller => 'restaurantes', :action => 'update'
+    #• DELETE /restaurantes/:id
+    #:controller => 'restaurantes', :action => 'destroy'
+    #• GET /restaurantes/:id/edit
+#:controller => 'restaurantes', :action => 'edit'
 end
