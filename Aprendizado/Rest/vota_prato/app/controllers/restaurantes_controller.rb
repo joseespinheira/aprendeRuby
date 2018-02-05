f2306cc16d55db373078d08338cbd96a4cfdd039
joseespinheira <joseespinheira@gmail.com>
2018-02-05 17:33:02 +0000

    Pagina 134, já estou cansando

--- Aprendizado/Rest/vota_prato/app/controllers/restaurantes_controller.rb
+++ Aprendizado/Rest/vota_prato/app/controllers/restaurantes_controller.rb
@@ -2,4 +2,7 @@ class RestaurantesController < ApplicationController
     def index
     @restaurantes = Restaurante.order :nome
     end
+    def show
+    @restaurante = Restaurante.find(params[:id])
+    end
 end
--- Aprendizado/Rest/vota_prato/db/seeds.rb
+++ Aprendizado/Rest/vota_prato/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# add 11 itens para teste:
+Restaurante.create(nome: "Fogão Quente", endereco: "Rua Aguiar Santos", especialidade: "Massa");
+Restaurante.create(nome: "China in Box", endereco: "Shopping Salvador", especialidade: "Chinesa");
+Restaurante.create(nome: "Ifood", endereco: "Internet", especialidade: "Delivery");
+Restaurante.create(nome: "Pizza na hora", endereco: "Rua Direta do capelão", especialidade: "Pizza");
+Restaurante.create(nome: "Rango quente", endereco: "Rua do milho", especialidade: "Quentinha");
+Restaurante.create(nome: "Comida Rapida", endereco: "Rua Arroz com Feijão", especialidade: "Rapida");
+Restaurante.create(nome: "Potato", endereco: "Shopping Salvador Norte", especialidade: "Batata");
+Restaurante.create(nome: "MC Donald's", endereco: "Shopping Bela vista", especialidade: "Hamburger");
+Restaurante.create(nome: "Burge King", endereco: "Av. Lucaia", especialidade: "Hamburger");
+Restaurante.create(nome: "Girrafas", endereco: "Av. Oceania", especialidade: "Pocaria");
+Restaurante.create(nome: "Bob's", endereco: "Shopping Itaigara", especialidade: "Hamburger");
